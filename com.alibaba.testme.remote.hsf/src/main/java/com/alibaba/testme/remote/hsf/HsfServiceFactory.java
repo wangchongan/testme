d9d91518d58c4ad1014fd5a25d2fd806a667bf14
cxfly <htzd007@gmail.com>
2013-05-24 18:07:58 +0800

    hsf模块加入

---
+++ com.alibaba.testme.remote.hsf/src/main/java/com/alibaba/testme/remote/hsf/HsfServiceFactory.java
@@ -0,0 +1,25 @@
+/**
+ * Project: com.alibaba.testme.remote.hsf
+ * 
+ * File Created at 2013-5-23
+ * $Id$
+ * 
+ * Copyright 1999-2100 Alibaba.com Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Alibaba Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Alibaba.com.
+ */
+package com.alibaba.testme.remote.hsf;
+
+/**
+ * TODO Comment of HsfServiceFactory
+ * @author lz
+ *
+ */
+public class HsfServiceFactory {
+
+}
