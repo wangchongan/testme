f6182a7c68808514387c7f3c44f5b895992fce7e
xiaopenzi0813 <xiaopenzi0813@sina.cn>
2013-05-24 13:32:47 +0800

    sqlmap等配置文件

--- com.alibaba.testme.domain/src/main/java/com/alibaba/testme/domain/dataobject/TestunitFlowDO.java
+++ com.alibaba.testme.domain/src/main/java/com/alibaba/testme/domain/dataobject/TestunitFlowDO.java
@@ -1,205 +1,211 @@
 package com.alibaba.testme.domain.dataobject;
+
 import java.util.Date;
+
 /**
  * TestunitFlowDO
  */
 public class TestunitFlowDO {
 
-	/**
-	 *id
-	 */
-	private Integer id;
-	   
-	/**
-	 *gmtCreate
-	 */
-	private Date gmtCreate;
-	   
-	/**
-	 *creator
-	 */
-	private String creator;
-	   
-	/**
-	 *gmtModified
-	 */
-	private Date gmtModified;
-	   
-	/**
-	 *modifier
-	 */
-	private String modifier;
-	   
-	/**
-	 *code
-	 */
-	private String code;
-	   
-	/**
-	 *name
-	 */
-	private String name;
-	   
-	/**
-	 *systemId
-	 */
-	private String systemId;
-	   
-	/**
-	 *tag
-	 */
-	private String tag;
-	   
-	/**
-	 *userId
-	 */
-	private Integer userId;
-	   
-	/**
-	 *picUrl
-	 */
-	private String picUrl;
-	   
-	/**
-	 *times
-	 */
-	private Integer times;
-	   
-	/**
-	 *envConfigRequired
-	 */
-	private String envConfigRequired;
-	   
-	/**
-	 *是否可用
-	 */
-	private String 是否可用;
-	   
-	/**
-	 *remark
-	 */
-	private String remark;
-	   
+    /**
+     * id
+     */
+    private Integer id;
 
+    /**
+     * gmtCreate
+     */
+    private Date    gmtCreate;
 
-	public void setId(Integer id){ 
-	   this.id = id; 
-	}
-	
-	public Integer getId(){ 
-	   return this.id; 
-	} 
+    /**
+     * creator
+     */
+    private String  creator;
 
-	public void setGmtCreate(Date gmtCreate){ 
-	   this.gmtCreate = gmtCreate; 
-	}
-	
-	public Date getGmtCreate(){ 
-	   return this.gmtCreate; 
-	} 
+    /**
+     * gmtModified
+     */
+    private Date    gmtModified;
 
-	public void setCreator(String creator){ 
-	   this.creator = creator; 
-	}
-	
-	public String getCreator(){ 
-	   return this.creator; 
-	} 
+    /**
+     * modifier
+     */
+    private String  modifier;
 
-	public void setGmtModified(Date gmtModified){ 
-	   this.gmtModified = gmtModified; 
-	}
-	
-	public Date getGmtModified(){ 
-	   return this.gmtModified; 
-	} 
+    /**
+     * code
+     */
+    private String  code;
 
-	public void setModifier(String modifier){ 
-	   this.modifier = modifier; 
-	}
-	
-	public String getModifier(){ 
-	   return this.modifier; 
-	} 
+    /**
+     * name
+     */
+    private String  name;
 
-	public void setCode(String code){ 
-	   this.code = code; 
-	}
-	
-	public String getCode(){ 
-	   return this.code; 
-	} 
+    /**
+     * systemId
+     */
+    private String  systemId;
 
-	public void setName(String name){ 
-	   this.name = name; 
-	}
-	
-	public String getName(){ 
-	   return this.name; 
-	} 
+    /**
+     * tag
+     */
+    private String  tag;
 
-	public void setSystemId(String systemId){ 
-	   this.systemId = systemId; 
-	}
-	
-	public String getSystemId(){ 
-	   return this.systemId; 
-	} 
+    /**
+     * userId
+     */
+    private Integer userId;
 
-	public void setTag(String tag){ 
-	   this.tag = tag; 
-	}
-	
-	public String getTag(){ 
-	   return this.tag; 
-	} 
+    /**
+     * picUrl
+     */
+    private String  picUrl;
 
-	public void setUserId(Integer userId){ 
-	   this.userId = userId; 
-	}
-	
-	public Integer getUserId(){ 
-	   return this.userId; 
-	} 
+    /**
+     * times
+     */
+    private Integer times;
 
-	public void setPicUrl(String picUrl){ 
-	   this.picUrl = picUrl; 
-	}
-	
-	public String getPicUrl(){ 
-	   return this.picUrl; 
-	} 
+    /**
+     * envConfigRequired
+     */
+    private String  envConfigRequired;
 
-	public void setTimes(Integer times){ 
-	   this.times = times; 
-	}
-	
-	public Integer getTimes(){ 
-	   return this.times; 
-	} 
+    /**
+     * 是否可用
+     */
+    private String  isActive;
 
-	public void setEnvConfigRequired(String envConfigRequired){ 
-	   this.envConfigRequired = envConfigRequired; 
-	}
-	
-	public String getEnvConfigRequired(){ 
-	   return this.envConfigRequired; 
-	} 
+    /**
+     * remark
+     */
+    private String  remark;
 
-	public void set是否可用(String 是否可用){ 
-	   this.是否可用 = 是否可用; 
-	}
-	
-	public String get是否可用(){ 
-	   return this.是否可用; 
-	} 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setRemark(String remark){ 
-	   this.remark = remark; 
-	}
-	
-	public String getRemark(){ 
-	   return this.remark; 
-	} 
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtCreate() {
+        return this.gmtCreate;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getCreator() {
+        return this.creator;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Date getGmtModified() {
+        return this.gmtModified;
+    }
+
+    public void setModifier(String modifier) {
+        this.modifier = modifier;
+    }
+
+    public String getModifier() {
+        return this.modifier;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setSystemId(String systemId) {
+        this.systemId = systemId;
+    }
+
+    public String getSystemId() {
+        return this.systemId;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return this.tag;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserId() {
+        return this.userId;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getPicUrl() {
+        return this.picUrl;
+    }
+
+    public void setTimes(Integer times) {
+        this.times = times;
+    }
+
+    public Integer getTimes() {
+        return this.times;
+    }
+
+    public void setEnvConfigRequired(String envConfigRequired) {
+        this.envConfigRequired = envConfigRequired;
+    }
+
+    public String getEnvConfigRequired() {
+        return this.envConfigRequired;
+    }
+
+    /**
+     * @return the isActive
+     */
+    public String getIsActive() {
+        return isActive;
+    }
+
+    /**
+     * @param isActive the isActive to set
+     */
+    public void setIsActive(String isActive) {
+        this.isActive = isActive;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getRemark() {
+        return this.remark;
+    }
 
 }
