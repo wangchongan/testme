232c462fd32e049e890258f870d49ab30286658e
xiaopenzi0813 <xiaopenzi0813@sina.cn>
2013-06-06 10:42:34 +0800

    配置管理

--- com.alibaba.testme.web/src/main/java/com/alibaba/testme/web/SystemConfigController.java
+++ com.alibaba.testme.web/src/main/java/com/alibaba/testme/web/SystemConfigController.java
@@ -23,7 +23,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,15 +31,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alibaba.testme.common.ibatispage.Page;
 import com.alibaba.testme.domain.dataobject.SystemDO;
-import com.alibaba.testme.domain.dataobject.SystemEnvDO;
-import com.alibaba.testme.domain.dataobject.SystemEnvDetailDO;
-import com.alibaba.testme.domain.dataobject.SystemRequirePropDO;
 import com.alibaba.testme.domain.enums.ConfigTypeEnum;
 import com.alibaba.testme.domain.query.SystemConfigQuery;
 import com.alibaba.testme.domain.vo.SystemConfigVO;
 import com.alibaba.testme.service.SystemEnvDetailService;
-import com.alibaba.testme.service.SystemEnvService;
-import com.alibaba.testme.service.SystemRequirePropService;
 import com.alibaba.testme.service.SystemService;
 
 /**
@@ -52,13 +46,9 @@ import com.alibaba.testme.service.SystemService;
 @RequestMapping(value = "/systemconfig/*")
 public class SystemConfigController {
     @Resource
-    private SystemService            systemService;
+    private SystemService          systemService;
     @Resource
-    private SystemEnvDetailService   systemEnvDetailService;
-    @Resource
-    private SystemRequirePropService systemRequirePropService;
-    @Resource
-    private SystemEnvService         systemEnvService;
+    private SystemEnvDetailService systemEnvDetailService;
     /**
      * 每页显示条数
      */
@@ -153,7 +143,7 @@ public class SystemConfigController {
         systemDO.setCreator("sys");
         systemDO.setModifier("sys");
         SystemDO entity = systemService.findByName(systemDO.getName());
-        if (entity != null && !entity.getId().equals(systemDO.getId())) {
+        if (entity != null) {
             resultMsg = "温馨提醒：该系统名称已经存在,name:" + systemDO.getName();
             model.addAttribute("resultMsg", resultMsg);
             return "systemconfig/editSystemConfig";
@@ -350,171 +340,4 @@ public class SystemConfigController {
         return "systemconfig/addSystemRequiredProp";
     }
 
-    /**
-     * 保存用户定义的系统必要参数
-     * 
-     * @param model
-     * @param request
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST)
-    public String saveSystemRequiredProp(Model model,
-                                         HttpServletRequest request,
-                                         @ModelAttribute("systemRequirePropDO") SystemRequirePropDO systemRequirePropDO) {
-        String resultMsg = null;
-        if (systemRequirePropDO == null || StringUtils.isBlank(systemRequirePropDO.getPropCode())
-                || StringUtils.isBlank(systemRequirePropDO.getPropName())
-                || systemRequirePropDO.getSystemId() == null) {
-            resultMsg = "温馨提醒：参数名称和所属系统不能为空！";
-            model.addAttribute("resultMsg", resultMsg);
-            return addSystemRequiredProp(model, request);
-        }
-        systemRequirePropDO.setCreator("sys");
-        systemRequirePropDO.setModifier("sys");
-        SystemRequirePropDO entity = systemRequirePropService.findByPropCodeAndSystemId(
-                systemRequirePropDO.getSystemId(), systemRequirePropDO.getPropCode());
-        if (entity != null) {
-            resultMsg = "温馨提醒：该系统下参数编码为：" + systemRequirePropDO.getPropCode() + "已经存在！";
-            model.addAttribute("resultMsg", resultMsg);
-            return addSystemRequiredProp(model, request);
-        }
-        Long result = systemRequirePropService.addSystemRequirePropDO(systemRequirePropDO);
-        if (result != null && result > 0) {
-            resultMsg = "温馨提醒：恭喜你！必要参数信息保存成功！";
-        } else {
-            resultMsg = "温馨提醒：必要参数信息保存失败！";
-        }
-        model.addAttribute("resultMsg", resultMsg);
-        return addSystemRequiredProp(model, request);
-    }
-
-    /**
-     * 进入配置必要参数页面
-     * 
-     * @param model
-     * @param request
-     * @return
-     */
-    @RequestMapping
-    public String configureSystemRequiredProp(Model model, HttpServletRequest request) {
-        //获取系统列表
-        List<SystemDO> systemDOList = systemService.findList(new SystemDO());
-        SystemRequirePropDO systemRequirePropDO = new SystemRequirePropDO();
-        systemRequirePropDO.setCreator("sys");
-        List<SystemRequirePropDO> entityList = systemRequirePropService
-                .findList(systemRequirePropDO);
-        model.addAttribute("systemDOList", systemDOList);
-        model.addAttribute("systemRequirePropDOList", entityList);
-
-        return "systemconfig/configureSystemRequiredProp";
-    }
-
-    /**
-     * 校验配置名称的唯一性
-     * 
-     * @param model
-     * @param request
-     * @param configName
-     * @param systemId
-     * @return
-     */
-    @RequestMapping
-    public String isExistsConfigName(Model model, HttpServletRequest request,
-                                     @RequestParam("configName") String configName) {
-        String validteConfigNameMsg = null;
-        SystemEnvDO entity = new SystemEnvDO();
-        entity.setConfigName(configName);
-        entity.setUserId(10L);
-        List<SystemEnvDO> systemEnvDOList = systemEnvService.findList(entity);
-        if (systemEnvDOList != null && systemEnvDOList.size() > 0) {
-            validteConfigNameMsg = "亲！该配置名称已经存在哦！";
-            model.addAttribute("validteConfigNameMsg", validteConfigNameMsg);
-        }
-        model.addAttribute("configName", configName);
-        return configureSystemRequiredProp(model, request);
-    }
-
-    /**
-     * 保存用户设置的系统必要参数值
-     * 
-     * @param model
-     * @param request
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST)
-    @Transactional
-    public String saveSystemEnv(Model model, HttpServletRequest request,
-                                @RequestParam("configName") String configName,
-                                @RequestParam("systemId") Long systemId) {
-        model.addAttribute("configName", configName);
-        model.addAttribute("systemId", systemId);
-        if (StringUtils.isBlank(configName) || systemId == null || systemId <= 0L) {
-            return goConfigureSystemRequiredProp("温馨提醒：配置名称和所属系统不能为空！", model, request);
-        }
-
-        String[] systemRequirePropIds = request.getParameterValues("systemRequirePropId");
-        if (systemRequirePropIds == null || systemRequirePropIds.length == 0) {
-            return goConfigureSystemRequiredProp("温馨提醒：参数信息为空，无需保存！", model, request);
-        }
-        //保存系统环境参数信息
-        Long result = saveSystemEnvDO(configName, systemId);
-        if (result == null || result <= 0L) {
-            return goConfigureSystemRequiredProp("温馨提醒：必要参数信息保存失败！", model, request);
-
-        }
-        //保存系统参数详情信息
-        for (String id : systemRequirePropIds) {
-            String propvalue = request.getParameter("propvalue_" + id);
-            String nullable = request.getParameter("nullable_" + id);
-            String propName = request.getParameter("propName_" + id);
-            String propCode = request.getParameter("propCode_" + id);
-            if (StringUtils.isBlank(nullable) || StringUtils.isBlank(propCode)
-                    || StringUtils.isBlank(propName)) {
-                return goConfigureSystemRequiredProp("温馨提醒：必要参数信息获取失败！请联系技术解决", model, request);
-            }
-            if ("N".equalsIgnoreCase(nullable) && StringUtils.isBlank(propvalue)) {
-                return goConfigureSystemRequiredProp("温馨提醒：不可为空的参数必须有值", model, request);
-            }
-
-            Long saveResult = saveSystemEnvDetailDO(propCode, propvalue, propName, result);
-            if (saveResult == null || saveResult <= 0L) {
-                systemEnvDetailService.deleteByEnvId(result);
-                systemEnvService.deleteSystemEnvDO(result);
-                return goConfigureSystemRequiredProp("温馨提醒：参数信息保存失败，请重试！", model, request);
-            }
-        }
-        return goConfigureSystemRequiredProp("温馨提醒：恭喜你！参数信息保存成功！", model, request);
-    }
-
-    private String goConfigureSystemRequiredProp(String resultMsg, Model model,
-                                                 HttpServletRequest request) {
-        model.addAttribute("resultMsg", resultMsg);
-        return configureSystemRequiredProp(model, request);
-    }
-
-    private Long saveSystemEnvDO(String configName, Long systemId) {
-        SystemEnvDO systemEnvDO = new SystemEnvDO();
-        systemEnvDO.setConfigName(configName);
-        systemEnvDO.setCreator("sys");
-        systemEnvDO.setModifier("sys");
-        systemEnvDO.setIsDefault("Y");
-        systemEnvDO.setSystemId(systemId);
-        systemEnvDO.setUserId(0L);
-
-        return systemEnvService.addSystemEnvDO(systemEnvDO);
-    }
-
-    private Long saveSystemEnvDetailDO(String propCode, String propvalue, String propName,
-                                       Long systemEnvId) {
-        SystemEnvDetailDO systemEnvDetailDO = new SystemEnvDetailDO();
-        systemEnvDetailDO.setCreator("sys");
-        systemEnvDetailDO.setModifier("sys");
-        systemEnvDetailDO.setPropKey(propCode);
-        systemEnvDetailDO.setPropValue(propvalue);
-        systemEnvDetailDO.setRemark(propName);
-        systemEnvDetailDO.setSystemEnvId(systemEnvId);
-        systemEnvDetailDO.setConfigType(ConfigTypeEnum.SYSTEM_REQUIRED.getKey());
-
-        return systemEnvDetailService.addSystemEnvDetailDO(systemEnvDetailDO);
-    }
 }
